7beef4ad26a6555551e4a092ab0b69cec496c6a6
Chousspb <chous_spb@mail.ru>
2023-04-05 12:41:39 +0300

    Initial Commit

---
+++ CompNameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompNameApp
+//
+//  Created by Admin on 05.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
